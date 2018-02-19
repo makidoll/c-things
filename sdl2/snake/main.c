9ff033442c49f463a9f59f36133262080c1324ce
Maki <mxmcube@gmail.com>
2018-02-19 18:14:30 +0000

    es-git-it

--- sdl2/snake/main.c
+++ sdl2/snake/main.c
@@ -1,9 +1,28 @@
-#include "game.h"
+#include <SDL2/SDL.h>
 
-int main(int argc, char* argv[]) {
+typedef struct {
+
+	unsigned char dir; // w,a,s,d 
+
+} Game;
+
+void init(Game* game) {
+
+}
+
+void update(Game* game) {
+	
+}
+
+int main(int argc, char const *argv[]) {
 	Game game;
 	init(&game);
-	while (game.state>0) update(&game);
-	deinit(&game);
+
+	while (game.state>0) {
+		update(&game);
+	}
+
+	SDL_DestroyWindow(game.window);
+	SDL_Quit()
 	return 0;
 }
