d2853fe25c1ba3b95780c5e67f0b7f34e56a4e28
Maki <mxmcube@gmail.com>
2018-06-09 19:45:23 +0100

    oki is done for now

--- sdl2/3d-engine/main.c
+++ sdl2/3d-engine/main.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include <SDL2/SDL.h>
+
+#define PI 3.14159265
 
 typedef struct { float x; float y; float z; } Vec3f;
 typedef struct { int x; int y; int z; } Vec3i;
+typedef struct { float x; float y; } Vec2f;
 
 SDL_Point project(Vec3f v) {
+	float z = (v.z>0)? 256/v.z: 0;
 	SDL_Point p = {
-		(int)(256 + v.x*32),
-		(int)(256 - v.y*32)
+		(int)((256 + v.x*z)),
+		(int)((256 - v.y*z))
 	};
 	return p;
 }
 
-int main (int argc, char* argv[]) {
+void rotateXZ(Vec3f* v, float r) {
+	float x = v->x; float z = v->z;
+	v->x = x*cos(r) - z*sin(r);
+	v->z = z*cos(r) + x*sin(r);
+}
+
+void rotateYZ(Vec3f* v, float r) {
+	float y = v->y; float z = v->z;
+	v->y = y*cos(r) - z*sin(r);
+	v->z = z*cos(r) + y*sin(r);
+}
+
+int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window* window = SDL_CreateWindow(
@@ -23,30 +40,90 @@ int main (int argc, char* argv[]) {
 	SDL_Renderer* renderer = SDL_CreateRenderer(
 		window, -1, SDL_RENDERER_ACCELERATED);
 
-	Vec3f pos = {0, 0, 0};
-	Vec3f rot = {0, 0, 0};
+	Vec3f pos = {0,0,0}; float posSpeed = 12;
+	Vec2f rot = {0,0}; float rotSpeed = 6; // only x,y
 
 	#include "obj.h" // import verts & faces
-	int cFaces = sizeof(faces)/sizeof(Vec3i);
 
+	int cFaces = sizeof(faces)/sizeof(Vec3i); // count faces
+	int cVerts = sizeof(verts)/sizeof(Vec3f); // count verts
+
+	Vec3f tVerts[cVerts]; // transformed verts
 	SDL_Point lines[cFaces*3*2]; // faces * (tris are 3) * (2 points is 1 line)
+
+	char keyDown[2] = {0}; // ws, ad, eq
 
 	int state = 1;
 	SDL_Event event;
+
+	//double time = 0;
+	double dt = 0;
+	clock_t dtc;
+
+	int locked = 1;
+
 	while (state) {
 		// update
-		int key = 0;
+		dtc = clock();
+
 		while(SDL_PollEvent(&event)) {
 			switch(event.type) {
 				case SDL_QUIT: state = 0; break;
 				case SDL_KEYDOWN:
 					switch(event.key.keysym.sym) {
-						case SDLK_w: key = 1; break;
-						case SDLK_a: key = 2; break;
-						case SDLK_s: key = 3; break;
-						case SDLK_d: key = 4; break;
+						case SDLK_w: keyDown[0]= 1; break;
+						case SDLK_s: keyDown[0]=-1; break;
+						case SDLK_a: keyDown[1]= 1; break;
+						case SDLK_d: keyDown[1]=-1; break;
+						case SDLK_e: keyDown[2]= 1; break;
+						case SDLK_q: keyDown[2]=-1; break;
 					}
 				break;
+				case SDL_KEYUP:
+					switch(event.key.keysym.sym) {
+						case SDLK_w: keyDown[0]=0; break;
+						case SDLK_s: keyDown[0]=0; break;
+						case SDLK_a: keyDown[1]=0; break;
+						case SDLK_d: keyDown[1]=0; break;
+						case SDLK_e: keyDown[2]=0; break;
+						case SDLK_q: keyDown[2]=0; break;
+
+						case SDLK_ESCAPE:
+							locked = (locked)? 0: 1;
+							break;
+					}
+				break;
+				case SDL_MOUSEMOTION:
+					if (!locked) break;
+					rot.x += event.motion.xrel*rotSpeed*dt;
+					rot.y -= event.motion.yrel*rotSpeed*dt;
+				break;
+			}
+		}
+
+		if (locked) {
+			SDL_SetRelativeMouseMode(SDL_TRUE);
+		} else {
+			SDL_SetRelativeMouseMode(SDL_FALSE);
+		}
+
+		if (locked) {		
+			if (keyDown[0]!=0) {
+				pos.x -= cos(rot.x+PI/2)*
+					posSpeed*keyDown[0]*dt;
+				pos.z += sin(rot.x+PI/2)*
+					posSpeed*keyDown[0]*dt;
+			}
+
+			if (keyDown[1]!=0) {
+				pos.x -= cos(rot.x)*
+					posSpeed*keyDown[1]*dt;
+				pos.z += sin(rot.x)*
+					posSpeed*keyDown[1]*dt;
+			}
+	
+			if (keyDown[2]!=0) {
+				pos.y += posSpeed*keyDown[2]*dt;
 			}
 		}
 
@@ -54,24 +131,41 @@ int main (int argc, char* argv[]) {
 		SDL_SetRenderDrawColor(renderer, 0,0,0,255);
 		SDL_RenderClear(renderer);
 
+		// transform verts
+		for (int iVerts=0; iVerts<cVerts; ++iVerts) {
+			tVerts[iVerts].x = verts[iVerts].x-pos.x;
+			tVerts[iVerts].y = verts[iVerts].y-pos.y;
+			tVerts[iVerts].z = verts[iVerts].z-pos.z;
+			rotateXZ(&tVerts[iVerts], rot.x);
+			rotateYZ(&tVerts[iVerts], rot.y);
+		}
 
+		// draw faces
+		for (int iFaces=0; iFaces<cFaces; ++iFaces) {
+			SDL_Point c0 = project(tVerts[faces[iFaces].x]); // xyz doesnt make sense
+			SDL_Point c1 = project(tVerts[faces[iFaces].y]); // here but whatever...
+			SDL_Point c2 = project(tVerts[faces[iFaces].z]);
 
-		// for (int iFaces=0; iFaces<cFaces; ++iFaces) {
-		// 	SDL_Point c0 = project(verts[faces[iFaces].x]); // xyz doesnt make sense
-		// 	SDL_Point c1 = project(verts[faces[iFaces].y]); // here but whatever...
-		// 	SDL_Point c2 = project(verts[faces[iFaces].z]);
-
-		// 	lines[iFaces*6    ] = c0; lines[iFaces*6 + 1] = c1;
-		// 	lines[iFaces*6 + 2] = c1; lines[iFaces*6 + 3] = c2;
-		// 	lines[iFaces*6 + 4] = c2; lines[iFaces*6 + 5] = c0;
-		// }
+			lines[iFaces*6    ] = c0; lines[iFaces*6 + 1] = c1;
+			lines[iFaces*6 + 2] = c1; lines[iFaces*6 + 3] = c2;
+			lines[iFaces*6 + 4] = c2; lines[iFaces*6 + 5] = c0;
+		}
 
 		SDL_SetRenderDrawColor(renderer, 255,255,255,255);
 		SDL_RenderDrawLines(renderer, lines, cFaces*3*2);
 
 		SDL_RenderPresent(renderer);
+
+		// delta time
+		#ifdef _WIN32
+			dt = ((float)(clock()-dtc)/1000000.0F)*1000; 
+		#else
+			dt = ((float)(clock()-dtc)/1000000000.0F)*1000; 
+		#endif
+		//time += dt;
 	}
 
+	// closing
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
